72e8b8457625f8b1b8ae94513ed04fe2c54bddfc
Mario Carballo Zama <afk@ellugar.co>
2021-01-29 11:25:01 -0600

    initial commit

---
+++ .github/site-checker/index.js
@@ -0,0 +1,111 @@
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const marked = require('marked');
+const axios = require('axios').default;
+
+function getFile() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('../../README.md', 'utf8', (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(data);
+    });
+  });
+}
+
+// TODO: Logic is wrong and returns duplicates
+function getElementsByType(obj, type = 'link') {
+  const res = [];
+  if (Array.isArray(obj)) {
+    obj.forEach((elem) => {
+      getElementsByType(elem, type).forEach((k) => {
+        res.push(k);
+      });
+    });
+  } else if (typeof obj === 'object' && obj !== null) {
+    Object.values(obj).forEach((elem) => {
+      if (obj.type === type) {
+        res.push(obj);
+      } else {
+        getElementsByType(elem, type).forEach((k) => {
+          res.push(k);
+        });
+      }
+    });
+  }
+  return [...new Set(res)];
+}
+
+async function getLinks(md) {
+  const tokens = marked.lexer(md);
+  const allLinks = getElementsByType(tokens).map(({ href }) => href);
+  return allLinks;
+}
+
+const validStatusCode = (code) => code >= 200 && code < 400;
+
+async function getStatusTotals(links) {
+  const total = links.length;
+  const errorMessages = [];
+  const getStatus = async (link) => {
+    try {
+      const res = await axios.get(link);
+      const valid = validStatusCode(res.status);
+      if (!valid) {
+        errorMessages.push(`${link} does not have a valid status code`);
+        return false;
+      }
+      return true;
+    } catch {
+      errorMessages.push(`${link} cound not be reached`);
+      return false;
+    }
+  };
+  const status = await Promise.all(links.map(getStatus));
+  const alive = status.filter(Boolean).length;
+  return {
+    alive,
+    total,
+    errorMessages,
+  };
+}
+
+async function getBadge(alive, total) {
+  const diff = total - alive;
+  let color = 'green';
+  if (diff > 0) {
+    color = 'yellow';
+  }
+  if (diff > 5) {
+    color = 'red';
+  }
+  const res = await axios(
+    `https://img.shields.io/badge/vivos-${alive}%2F${total}-${color}`,
+  );
+  return res.data;
+}
+
+(async function main() {
+  const md = await getFile();
+  const links = await getLinks(md);
+  const { alive, total, errorMessages } = await getStatusTotals(links);
+  const badgeSvg = await getBadge(alive, total);
+
+  const badgePath = '../../_badges/reachable-site.svg';
+  const errorFilePath = '../../_badges/reachable-site-errors.txt';
+  const errorMessage = errorMessages.join('\r\n');
+  if (errorMessages.length > 0) {
+    console.error('errorMessage', errorMessage);
+  }
+  const writeFileAsync = promisify(fs.writeFile);
+  const mkdirAsync = promisify(fs.mkdir);
+  await mkdirAsync(path.dirname(badgePath), { recursive: true });
+  await writeFileAsync(badgePath, badgeSvg);
+  await writeFileAsync(errorFilePath, errorMessage);
+  process.exit(0);
+}()).catch((e) => {
+  console.error(`Error: ${e.message}`);
+  process.exit(1);
+});
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# estudios
+# Estudios
 Lista de estudios
+
+## Mexico
+
+- [Hyperbeard Games](https://hyperbeard.com/)
+- [Bromio](https://www.bromio.com.mx/)
+- [Ogre Pixel](http://ogrepixel.com/)
+- [Big Mini Geek](http://www.bigminigeek.com/)
+- [Mecha Studios](https://www.mechastudios.com/)
