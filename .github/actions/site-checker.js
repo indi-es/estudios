81e1495422b871b2222f203ff95245c3167102d4
afk <afk@ellugar.co>
2021-07-22 10:46:32 -0500

    Multiple fixes (#94)
    * Multiple fixes
    
    - Change items with key `web` -> `website`
    - Update badge link to error file
    - Format errors on save
    
    * add date to error reporting

--- .github/actions/site-checker.js
+++ .github/actions/site-checker.js
@@ -24,7 +24,11 @@ async function getStatusTotals(links) {
       const valid = validStatusCode(res.status);
       console.info(`[${valid ? 'active' : 'dead'} - ${res.status}] ${link}`);
       if (!valid) {
-        errorMessages.push({ link, status: res.status });
+        errorMessages.push({
+          link,
+          status: res.status,
+          date: new Date().toISOString(),
+        });
       }
       return valid;
     } catch (e) {
@@ -67,7 +71,7 @@ async function getBadge(alive, total) {
 
   const badgePath = '../../_badges/reachable-site.svg';
   const errorFilePath = '../../_badges/reachable-site-errors.json';
-  const errorMessage = JSON.stringify(errorMessages);
+  const errorMessage = JSON.stringify(errorMessages, null, 2);
   if (errorMessages.length > 0) {
     console.error('errorMessage', errorMessage);
   }
